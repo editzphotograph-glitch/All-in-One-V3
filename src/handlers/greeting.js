da123319ec1b508dd81f5dfd7986c0f36404b37f
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2025-10-16 23:00:00 +0530

    Fix greeting content concatenation

--- src/handlers/greeting.js
+++ src/handlers/greeting.js
@@ -56,6 +56,7 @@ const parse = async (content, member, inviterData = {}) => {
 const buildGreeting = async (member, type, config, inviterData) => {
   if (!config) return;
   let content;
+  content += `${member.toString()} `;
 
   // build content
   if (config.content) content = await parse(config.content, member, inviterData);
