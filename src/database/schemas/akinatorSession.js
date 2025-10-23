cef8a46aab87b4f83fc81e014d59f320f5b235fe
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2025-10-23 22:27:26 +0530

    Update akinatorSession.js

--- src/database/schemas/akinatorSession.js
+++ src/database/schemas/akinatorSession.js
@@ -1,16 +1,53 @@
 const mongoose = require("mongoose");
+const { CACHE_SIZE } = require("@root/config.js");
+const FixedSizeMap = require("fixedsize-map");
 
-const AkinatorSessionSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  category: { type: String, required: true },
-  startedAt: { type: Date, default: Date.now },
-  finishedAt: { type: Date },
-  result: {
-    name: { type: String },
-    description: { type: String },
-    image: { type: String },
-    probability: { type: Number },
+const cache = new FixedSizeMap(CACHE_SIZE.AKINATOR || 200);
+
+const Schema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    username: String,
+    category: { type: String, required: true },
+    result: {
+      name: String,
+      description: String,
+      image: String,
+      probability: Number,
+    },
+    finishedAt: { type: Date, default: Date.now },
   },
-});
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 
-module.exports = mongoose.model("AkinatorSession", AkinatorSessionSchema);
+const Model = mongoose.model("akinatorSession", Schema);
+
+module.exports = {
+  /**
+   * Create a new session
+   * @param {object} data
+   */
+  createSession: async (data) => {
+    const newSession = new Model(data);
+    await newSession.save();
+    cache.add(data.userId, newSession);
+    return newSession;
+  },
+
+  /**
+   * Get last session
+   * @param {string} userId
+   */
+  getSession: async (userId) => {
+    const cached = cache.get(userId);
+    if (cached) return cached;
+    const session = await Model.findOne({ userId }).sort({ finishedAt: -1 });
+    if (session) cache.add(userId, session);
+    return session;
+  },
+};
