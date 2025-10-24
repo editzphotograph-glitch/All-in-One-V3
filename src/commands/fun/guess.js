dd3ac6f05b7a0fd4f810aeaf178172814cbf7537
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2025-10-25 01:10:04 +0530

    Update guess.js

--- src/commands/fun/guess.js
+++ src/commands/fun/guess.js
@@ -10,7 +10,7 @@ const {
   ComponentType,
 } = require("discord.js");
 const { Akinator } = require("@aqul/akinator-api");
-const AkiSession = require("../../src/database/schemas/AkiSession");
+const AkiSession = require("../../database/schemas/AkiSession");
 
 /**
  * @type {import("@structures/Command")}
